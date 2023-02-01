e74da12cb0210ad769a91641516401de42027422
JesusFerVid <jesusfervid94@gmail.com>
2023-02-01 17:37:57 +0100

    Initial commit

---
+++ src/main/java/com/jesusfervid/dotify/model/User.java
@@ -0,0 +1,23 @@
+package com.jesusfervid.dotify.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", nullable = false)
+	private Long id;
+
+	@Column(name = "username", nullable = false)
+	private String username;
+}
---
+++ src/test/java/com/jesusfervid/dotify/AppTests.java
@@ -0,0 +1,13 @@
+package com.jesusfervid.dotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
